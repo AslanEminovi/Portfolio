c9018c5bacaf873d51e55d61f3c27bb100388241
Aslan <aslan.eminovi@istanbulticaret.edu.tr>
2024-09-28 17:59:21 +0300

    Add current state of project including .htaccess and server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
- 
-const port = parseInt(process.env.PORT || '3000', 10)
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
- 
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
+    const host = req.headers.host
+
+    // Redirect non-www to www
+    if (host === 'aslaneminovi.com.tr') {
+      res.writeHead(301, { Location: `http://www.aslaneminovi.com.tr${req.url}` })
+      res.end()
+      return
+    }
+
     handle(req, res, parsedUrl)
   }).listen(port)
- 
+
   console.log(
     `> Server listening at http://localhost:${port} as ${
       dev ? 'development' : process.env.NODE_ENV
